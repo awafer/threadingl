27afa4fbf9364d840baf4d605d16df81ecefd42a
mugmickey <mugmickey@gmail.com>
2021-07-18 00:28:13 +0800

    minor changes

--- threadingl/Program.cs
+++ threadingl/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace threadingl
 {
     class Program
     {
         static int y;
+        static object locker = new object();
 
         static void Main(string[] args)
         {
@@ -13,45 +15,65 @@ namespace threadingl
 
             Thread worker = new Thread(() =>
             {
+
                 // receive message.
                 while (true)
                 {
-                    y = Console.CursorTop;
-
-                    // Force a scroll if we're at the end of the buffer
-                    if (y == Console.BufferHeight - 1)
+                    //Thread.Sleep(2000);
+                    var t = Task.Run(async delegate
                     {
-                        Console.WriteLine();
-                        Console.SetCursorPosition(0, --y);
-                    }
+                        await Task.Delay(2000);
+                    });
 
-                    Thread.Sleep(2000);//TODO:change to timer
+                    t.Wait();
 
-                    if (Console.CursorTop != y)//for multiple input, if the user input something, y will change.
+                    lock (locker)
                     {
-                        continue;
+                        y = Console.CursorTop;
+
+                        // Force a scroll if we're at the end of the buffer
+                        if (y == Console.BufferHeight - 1)
+                        {
+                            Console.WriteLine();
+                            Console.SetCursorPosition(0, --y);
+                        }
+
+                        //if (Console.CursorTop != y)//for multiple input, if the user input something, y will change.
+                        //{
+                        //    continue;
+                        //}
+
+                        int x = Console.CursorLeft;
+                        Console.MoveBufferArea(0, y, Console.WindowWidth, 1, 0, y + 1);//move the current line to next line, if the console start a new line, will move only the line at the bottom.
+                        Console.SetCursorPosition(0, y);//back to previous line
+                        Console.Write("I just waited 2 seconds.");
+                        Console.SetCursorPosition(x, y + 1);//move the cursor to where it was
+
+                        //Console.WriteLine("doing things ...");
                     }
-
-                    int x = Console.CursorLeft;
-                    Console.MoveBufferArea(0, y, Console.WindowWidth, 1, 0, y + 1);//move the current line to next line, if the console start a new line, will move only the line at the bottom.
-                    Console.SetCursorPosition(0, y);//back to previous line
-                    Console.Write("I just waited 2 seconds.");
-                    Console.SetCursorPosition(x, y + 1);//move the cursor to where it was
-
-                    //Console.WriteLine("doing things ...");
                 };
             });
 
             worker.Start();
 
             string input;
-            do
+            while(true)
             {
-                Console.Write("Input(enter [empty] to quit): ");
+                lock(locker)
+                {
+                    Console.Write("Input(enter [empty] to quit): ");
+                }
                 input = Console.ReadLine();
-                Console.WriteLine("You typed: " + input);
+                if(String.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+                lock (locker)
+                {
+                    Console.WriteLine("You typed: " + input);
+                }
                 //line_input++;
-            } while (!String.IsNullOrWhiteSpace(input));
+            }
 
             System.Environment.Exit(0);
         }
